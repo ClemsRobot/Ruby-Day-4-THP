a0779ebb16cf302407f55d7dfdcc720008db9a9f
ClemsRobot <cle.castel@gmail.com>
2018-10-04 20:35:43 +0200

    Commit et pushé par Clément

---
+++ journalist.rb
@@ -0,0 +1,109 @@
+
+def array #upload des arrays
+@emails = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux",
+    "@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi",
+    "@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg",
+    "@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau",
+    "@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital",
+    "@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis",
+    "@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak",
+    "@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard",
+    "@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos",
+    "@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose",
+    "@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier",
+    "@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar",
+    "@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara",
+    "@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach",
+    "@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+return @emails
+end
+
+def nombredejournalistes(array_principal)
+puts "Ceci représente le nombre de journalistes dans le tableau : #{array_principal.length}" # Le "# sert a appeler la variable "
+end # fonction pour le nombre de journalistes dans le tableau
+
+def chiffre(array_two)
+true if Integer(array_two) rescue false
+end #Fonction qui va servir après pour éviter l'erreur avec les nombres entiers (définir valeur numérique)
+
+def contenance_chiffre(array_principal)
+compteur = 0 # Ruby commence toujours à 0 (0=1; 1=2; 2=3 ect...) On utilise le compteur pour avanccer 1 à 1 dans la boucle https://fr.wikiversity.org/wiki/Ruby/Boucles
+array_principal.each do |variable|
+variable.each_char do |variable1| # https://ruby-doc.org/core-2.1.0/String.html pour comprendre char = string
+if chiffre(variable1)
+compteur += 1 # Pour compter emails +1 à chaque validation
+break
+end
+end
+end
+puts "#{compteur} des emails contiennent au moins un chiffre "  # Le "# sert a appeler la variable "
+return compteur
+end # Compte les emails qui contienne au moins un chiffre
+
+def nombre_aude(array_principal)
+compteur = 0
+array_principal.each do |variable|
+if variable.partition("aude")[1]=="aude"
+compteur += 1
+end
+end
+puts "#{compteur} emails contiennent 'aude' dans les données."  # Le "# sert a appeler la variable "
+return compteur
+end # Fonction pour compter le nombre de fois le nom "aude"
+
+def commence_majuscule(array_principal)
+compteur = 0
+array_principal.each do |variable|
+if variable[1]==variable[1].upcase # Majuscule tuto : https://ruby-doc.org/core-2.2.3/String.html
+compteur += 1
+end
+end
+puts "#{compteur} emails commencent par une majuscule." # Le "# sert a appeler la variable "
+return compteur
+end # Fonction pour voir combien d'emails commencent par une majuscule
+
+def majuscule_dansnom(array_principal)
+compteur = 0
+array_principal.each do |variable|
+1.step(variable.length-1,1) do |i|
+if variable[i]!="@" && variable[i] != "_" && !chiffre(variable[i])
+if variable[i]==variable[i].upcase #Majuscule
+compteur +=1
+break
+end
+end
+end
+end
+puts "#{compteur} emails contiennent au moins une majuscule"  # Le "# sert a appeler la variable "
+return compteur
+end # Fonction pour compter les emails qui contiennent une majuscule (au moins)
+
+def nombre_tiretdubas (array_principal)
+
+compteur = 0 # on commence à 0 pour éviter confusion avec le langage ruby (0=1)
+array_principal.each do |variable|
+variable.each_char do |variable1|
+if variable1=="_" # Si variable contient un tiret du bas
+compteur += 1
+end
+end
+end
+puts "#{compteur} emails contiennent '_' "  # Le "# sert a appeler la variable "
+return compteur
+end # Cette fonction sert à chercher le nombre de tiret du bas dans tous les emails.
+
+def tri_alpha(array_principal)
+array_principal = array_principal.sort_by {|x| x.downcase} # aide pour tri alphabetique https://stackoverflow.com/questions/37247219/how-to-convert-lowercase-string-to-uppercase-and-vice-versa
+puts "Voici la list des emails par ordre alphabetique : #{array_principal.sort_by {|x| x.downcase}}"
+return array_principal
+end # Cette fonction sert à trier par ordre alphabetique
+
+array
+nombredejournalistes(@emails)
+contenance_chiffre(@emails)
+nombre_aude(@emails)
+commence_majuscule(@emails)
+majuscule_dansnom(@emails)
+nombre_tiretdubas(@emails)
+tri_alpha(@emails)
