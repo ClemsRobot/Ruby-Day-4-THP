56a5f70b29cfa8f499e1e6019aa275b31d1fffea
ClemsRobot <cle.castel@gmail.com>
2018-10-04 20:54:08 +0200

    #4

--- journalist.rb
+++ journalist.rb
@@ -1,5 +1,6 @@
 
 def array #upload des arrays
+
 @emails = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux",
     "@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi",
     "@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg",
@@ -99,7 +100,7 @@ puts "Voici la list des emails par ordre alphabetique : #{array_principal.sort_b
 return array_principal
 end # Cette fonction sert à trier par ordre alphabetique
 
-array
+array # Rappelle les données pour raccord
 nombredejournalistes(@emails)
 contenance_chiffre(@emails)
 nombre_aude(@emails)
